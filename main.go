342bd1924cbb69c68d1c1206c5588e3759f514f5
windowsboy111 <wboy111@outlook.com>
2023-03-05 02:03:24 +0800

    feat: wip init code for git

--- main.go
+++ main.go
@@ -2,25 +2,65 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/go-martini/martini"
 )
+
+var fs = memfs.New() // I need help on how to actually interact with the fs aaa
+var gitstore = memory.NewStorage()
+
+func init() {
+	if _, err := os.Stat("./terra/"); os.IsNotExist(err) {
+		log.Println("Cloning repo...")
+		_, e := git.Clone(gitstore, fs, &git.CloneOptions{
+			URL: "git@github.com:terrapkg/terra.git",
+		})
+		if e != nil {
+			log.Fatal(e);
+			os.Exit(1)
+		}
+	} else {
+		r, e := git.Open(gitstore, fs)
+		if e != nil {
+			log.Fatal(e)
+			os.Exit(1)
+		}
+		wt, e := r.Worktree()
+		if e != nil {
+			log.Fatal(e)
+			os.Exit(1)
+		}
+		switch wt.Pull(&git.PullOptions{}) {
+			case nil, git.NoErrAlreadyUpToDate: break
+			default: {
+				log.Fatal(e)
+				os.Exit(1)
+			}
+		}
+	}
+}
 
 func gh(req *http.Request) int {
 	if req.Header.Get("X-GitHub-Event") != "push" {
 		return 400
 	}
+
+	type GhCommit struct {
+		modified []string
+	}
 	type GhResp struct {
-		ref string
-		compare string
+		ref     string
+		commits []GhCommit
 	}
 	decoder := json.NewDecoder(req.Body)
 	var resp GhResp
 	decoder.Decode(&resp)
-	// resp.compare + ".diff"
-	// parse diff to find out changed files?
-	// or use git directly? dunno
 	return 204
 }
 
